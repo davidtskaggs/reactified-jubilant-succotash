8ba0545859d2fbaf4a2d19a0ab1da772a1e40fdb
[David <[david@d3-nyc.com]>
2018-07-25 15:28:38 -0400

    Update form

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -8,22 +8,14 @@ class Contact extends React.Component {
           Contact
         </h1>
         <form action="https://formspree.io/davidtskaggs@gmail.com" method="POST">
-        <div>
-          <input className="input" type="text" name="name" id="name" placeholder=" Full Name" />
-        </div>
-        <div>
+        <input className="input" type="text" name="name" id="name" placeholder=" Full Name" />
+
           <input type="text" className="input" name="telephone" id="telephone" placeholder=" Telephone" />
-        </div>
-        <div>
+
           <input type="email" className="input" name="_replyto" id="email" placeholder=" Email" />
-        </div>
-        <div>
           <input type="text" className="input" name="subject" id="subject" placeholder=" Subject" />
-        </div>
-        <div>
           <textarea type="text" id="text-area" className="input" rows="5" name="message" placeholder=" Message">
           </textarea>
-        </div>
         <button type="submit" value="Send" className="button btn-block">Submit</button>
       </form>
     </div>
