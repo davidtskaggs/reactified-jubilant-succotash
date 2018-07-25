66cfc7b0d456b7c7dc81a3039c870bfe822f518d
[David <[david@d3-nyc.com]>
2018-07-25 15:10:33 -0400

    Add contact form

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,6 +1,5 @@
 var React = require('react');
 
-
 class Contact extends React.Component {
   render() {
     return(
@@ -8,6 +7,25 @@ class Contact extends React.Component {
         <h1>
           Contact
         </h1>
+        <form action="https://formspree.io/davidtskaggs@gmail.com" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" name="name" id="name" placeholder="Full Name" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" name="telephone" id="telephone" placeholder="Telephone" />
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control input-lg" name="_replyto" id="email" placeholder="Email" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" name="subject" id="subject" placeholder="Subject" />
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control input-lg"  rows="5" name="message" id="message" placeholder="Message">
+                        </textarea>
+                    </div>
+                    <button type="submit" value="Send" class="btn btn-block">Submit</button>
+                </form>
       </div>
     )
   }
