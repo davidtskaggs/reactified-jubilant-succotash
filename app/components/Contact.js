e8f7b2257f41ddc66adfa55ce12e5165ab0d5ccf
[David <[david@d3-nyc.com]>
2018-07-25 15:25:56 -0400

    Updates to form and styling

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -8,25 +8,25 @@ class Contact extends React.Component {
           Contact
         </h1>
         <form action="https://formspree.io/davidtskaggs@gmail.com" method="POST">
-                    <div class="form-group">
-                        <input type="text" class="form-control input-lg" name="name" id="name" placeholder="Full Name" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control input-lg" name="telephone" id="telephone" placeholder="Telephone" />
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control input-lg" name="_replyto" id="email" placeholder="Email" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control input-lg" name="subject" id="subject" placeholder="Subject" />
-                    </div>
-                    <div class="form-group">
-                        <textarea type="text" class="form-control input-lg"  rows="5" name="message" id="message" placeholder="Message">
-                        </textarea>
-                    </div>
-                    <button type="submit" value="Send" class="btn btn-block">Submit</button>
-                </form>
-      </div>
+        <div>
+          <input className="input" type="text" name="name" id="name" placeholder=" Full Name" />
+        </div>
+        <div>
+          <input type="text" className="input" name="telephone" id="telephone" placeholder=" Telephone" />
+        </div>
+        <div>
+          <input type="email" className="input" name="_replyto" id="email" placeholder=" Email" />
+        </div>
+        <div>
+          <input type="text" className="input" name="subject" id="subject" placeholder=" Subject" />
+        </div>
+        <div>
+          <textarea type="text" id="text-area" className="input" rows="5" name="message" placeholder=" Message">
+          </textarea>
+        </div>
+        <button type="submit" value="Send" className="button btn-block">Submit</button>
+      </form>
+    </div>
     )
   }
 }
