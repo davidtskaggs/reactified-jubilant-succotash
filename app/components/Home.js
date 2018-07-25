fd7a32a1e4d3e7ba1b0576770beb6675ffd442f5
[David <[david@d3-nyc.com]>
2018-07-25 11:30:50 -0400

    Add home updates

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,9 @@ class Home extends React.Component {
   render() {
     return(
       <div className='home-container'>
-        <h1>Home</h1>
+        <h1>Audrey Tatiana Donald</h1>
+        <h2>Digital Marketer</h2>
+        <h3>NYC</h3>
       </div>
     )
   }
